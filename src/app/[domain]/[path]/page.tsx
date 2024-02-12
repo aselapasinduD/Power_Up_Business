7a98f4874cd6ac52102581a6bb0e94569d8ccfda
aselapasinduD <asela.pasindu.dias17@gmail.com>
2024-02-12 23:48:46 +0530

    finish site page and setup middleware.ts for app naviagtion

---
+++ src/app/[domain]/[path]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+    return(
+        <div>
+            Path
+        </div>
+    );
+}
+
+export default Page;
---
+++ src/app/site/layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navigation from "@/components/site/navigation";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+const layout = ({ children }: { children:React.ReactNode }) => {
+    return(
+        <ClerkProvider
+            appearance={{baseTheme: dark}}
+        >
+            <main className="h-full">
+                <Navigation />
+                {children}
+            </main>
+        </ClerkProvider>
+    )
+}
+
+export default layout;
